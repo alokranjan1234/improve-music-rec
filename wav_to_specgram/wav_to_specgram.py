6f05610e13a883f3566e6fb32f6ba1bdca8bdab7
Jasper Louie <jlouie95@gmail.com>
2016-10-19 01:33:53 -0700

    Add pickle func default for wav_to_specgram
    Use pickling instead of txt file wariting to conserve memory
    and for easier retrieval of data. Also modularize specgram
    production in case we do not want to write specgram to disk

--- wav_to_specgram/wav_to_specgram.py
+++ wav_to_specgram/wav_to_specgram.py
@@ -3,6 +3,12 @@ import sys
 from matplotlib import mlab
 from wavio import *
 
+def gen_specgram(audio_filename):
+	"""generates spectogram from audio_filename"""
+	samplerate, sampwidth, nchannels, samples = readwav(audio_filename) # Read the audio file
+	samples = samples.T[0]	# Get the data for the channel (assuming this is a stem and we want the only existing channel)
+	return mlab.specgram(samples, Fs=samplerate) # Compute the spectrogram
+
 """
 - Compute and write out the spectrogram for a .wav audio file.
 - Format of the spectrogram: each row corresponds to a time step, and each column is the intensity of
@@ -10,10 +16,8 @@ from wavio import *
 - Individual values are delimited by spaces, and rows are delimited by newlines.
 - Warning: writing out spectrograms for large audio files will take a long time and produce a very large output file.
 """
-def write_specgram(audio_filename, out_filename):
-	samplerate, sampwidth, nchannels, samples = readwav(audio_filename) # Read the audio file
-	samples = samples.T[0]	# Get the data for the channel (assuming this is a stem and we want the only existing channel)
-	Pxx, freqs, bins = mlab.specgram(samples, Fs=samplerate) # Compute the spectrogram
+def write_specgram_as_txt(audio_filename, out_filename):
+	Pxx, freqs, bins = gen_specgram(audio_filename)
 	out = open(out_filename, "w");	# Write out the spectrogram
 	for i in range(len(Pxx)):
 		for j in range(len(Pxx[i])):
@@ -21,8 +25,14 @@ def write_specgram(audio_filename, out_filename):
 		out.write("\n")
 	out.close()
 
+def write_specgram_as_pickle(audio_filename, out_filename):
+	Pxx, freqs, bins = gen_specgram(audio_filename)
+	Pxx.dump(out_filename)
+
+
 if (__name__ == "__main__"):
 	if (len(sys.argv) != 3):
 		print("usage: wav_to_specgram.py <input .wav file> <output file>")
 		sys.exit()
-	write_specgram(sys.argv[1], sys.argv[2])
+	# write_specgram_as_txt(sys.argv[1], sys.argv[2])
+	write_specgram_as_pickle(sys.argv[1], sys.argv[2])
