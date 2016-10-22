878dfe0aeb4fd8cd26c7c8c602b338de85f08bc2
George He <georgewho96@gmail.com>
2016-10-22 15:20:42 -0700

    Update run.sh

--- midi_to_wav/run.sh
+++ midi_to_wav/run.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # example use: bash run.sh <$1> <$2> first parameter should be file directory of input file, second should be file directory of output files
 # Must have timidity installed: brew install timidity
-cnt=0
-replace=parquet
 makedir="mkdir $2"
+eval $makedir
 for file in `ls $1`
 do
 newfile="$2/${file}.wav"
-inst="timidity $0/$file -Ow -o $newfile"
-echo $inst
+inst="timidity $1/$file -Ow -o $newfile"
+eval $inst
 ((cnt++))
 done
 echo
